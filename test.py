265b915141b3924da12c0ff86de67c7701f207cc
tanbin-hasnat-shehab <85411927+tanbin-hasnat-shehab@users.noreply.github.com>
2021-06-25 17:13:22 +0600

    Update test.py

--- test.py
+++ test.py
@@ -5,10 +5,24 @@ import matplotlib.pyplot as plt
 
    
 obj=ss.beam([[1,10],[3,9],[4,8],[7,2],[9,12]],15,7)
-print(obj.Reactions())
-x_x,y_y=obj.bmd()
+#beam method needs 3 arguments.....1) an array of [point load position from left end,value of point load] 2) length of the beam  3)value of uniformly distributed load
 
-plt.plot(x_x,y_y)
+# printing force reactions RA,RB
+print(obj.Reactions())
+
+#bmd method return two array of point co ordinate of x and y to draw the Bending moment diagram or to do your stuff..
+bmd_x,bmd_y=obj.bmd()
+
+#sfd method return two array of point co ordinate of x and y to draw the Shear force diagram or to do your stuff..
+sfd_x,sfd_y=obj.sfd()
+
+
+#plotting sfd bmd
+plt.plot(bmd_x,bmd_y)
 plt.show()
+
+plt.plot(sfd_x,sfd_y)
+plt.show()
+
 
 
